40b780aa64a137aedb023a539b04ba68376f8d48
Erick <codigoflex@gmail.com>
2018-12-21 22:40:04 -0600

    segundo punto parte del menu y slider terminado

--- js/mi.js
+++ js/mi.js
@@ -8,3 +8,23 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('.slider').slider();
 });
+
+
+
+$(document).on('ready', mifuncion);
+function mifuncion(){
+    $('#menu a').on('click', laseccion);
+
+}
+function laseccion(){
+    var seccion =$(this).attr('href');
+    $('body, html').animate({
+        scrollTop:$(seccion).offset().top
+    }, 800);
+    return false;
+}            
+
+$(document).ready(function(){
+    // TABS
+    $('ul.tabs').tabs();
+});
